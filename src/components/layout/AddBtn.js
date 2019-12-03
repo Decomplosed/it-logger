8ffde8bcafd4320bdb6ac0663d18a6d3f4e9c229
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-03 23:55:04 +0100

    created a div within the a href for styling out the button

--- src/components/layout/AddBtn.js
+++ src/components/layout/AddBtn.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const AddBtn = props => {
   return (
-    <div>
-
+    <div className='fixed-action-btn'>
+      <a href='#add-log-modal' className='btn-floating btn-large blue darken-2 modal-trigger'>
+        <i className='large material-icons'>add</i>
+      </a>
     </div>
   )
 }
