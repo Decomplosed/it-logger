67fa405981c95dc07b86bec30b4e05a7faa1333e
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-05 16:39:21 +0100

    added cases for GET_LOGS and LOGS_ERROR

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -13,10 +13,22 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case GET_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+        loading: false
+      }
     case SET_LOADING:
       return {
         ...state,
         loading: true
+      }
+    case LOGS_ERROR:
+      console.log(action.payload)
+      return {
+        ...state,
+        error: action.payload
       }
     default:
       return state
