0b89c3c2d7f434407c9e9c78e6d36e3164449f76
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-05 13:54:01 +0100

    fixed errors inside store.js

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
-import { createStore, applyMidleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import thunk from 'react-thunk'
+import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
 const initialState = {}
@@ -10,7 +10,7 @@ const middleware = [thunk]
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMidleware(...middleware))
+  composeWithDevTools(applyMiddleware(...middleware))
 )
 
 export default store
