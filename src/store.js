61993b1a0a858e7439724cdd74e6ce72a5dcf648
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-05 12:19:46 +0100

    created const for initialState and middleWare with thunk

--- src/store.js
+++ src/store.js
@@ -1,4 +1,10 @@
 import { createStore, applyMidleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'react-thunk'
+import rootReducer from './reducers'
 
+const initialState = {}
+
+const middleWare = [thunk]
+
+const store = createStore()
