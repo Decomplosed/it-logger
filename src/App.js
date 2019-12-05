e296a818555558275c8cdf7fd4516e44f0605ec8
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-05 12:23:12 +0100

    imported Provider to the main App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import AddLogModal from './components/logs/AddLogModal'
 import EditLogModal from './components/logs/EditLogModal'
 import AddTechModal from './components/techs/AddTechModal'
 import TechListModal from './components/techs/TechListModal'
+import { Provider } from 'react-redux'
+
 
 import 'materialize-css/dist/css/materialize.min.css'
 import M from 'materialize-css/dist/js/materialize.min.js'
 import './App.css';
 
 const App = () => {
+
   useEffect(() => {
     M.AutoInit()
   })
