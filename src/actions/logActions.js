b4ee729455997a629ee4a5d4eb11d796832c531f
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-06 15:55:04 +0100

    finished creating addLog method

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -38,15 +38,21 @@ export const getLogs = () => async dispatch => {
   }
 }
 
-export const addLog = () => async dispatch => {
+export const addLog = (log) => async dispatch => {
   try {
     setLoading()
 
-    const res = await fetch('/logs')
+    const res = await fetch('/logs', {
+      method: 'POST',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
     const data = await res.json()
 
     dispatch({
-      type: GET_LOGS,
+      type: ADD_LOG,
       payload: data
     })
   } catch (err) {
