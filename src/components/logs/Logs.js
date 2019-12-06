a89f17efb0710470d16de3d1864b904d63c4b149
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-06 15:41:17 +0100

    bring the getLogs method to the app using connect

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -3,16 +3,16 @@ import { connect } from 'react-redux'
 import LogItem from './LogItem'
 import Preloader from '../layout/Preloader'
 import PropTypes from 'prop-types'
+import { getLogs } from '../../actions/logActions'
 
+const Logs = ({ log: { logs, loading } }) => {
 
-const Logs = () => {
-
-  useEffect(({ log: { logs, loading } }) => {
+  useEffect(() => {
     getLogs()
     //eslint-disable-next-line
   }, [])
 
-  if (loading) {
+  if (loading || logs === null) {
     return <Preloader />
   }
 
@@ -36,4 +36,4 @@ const mapStateToProps = state => ({
   log: state.log
 })
 
-export default connect(mapStateToProps)(Logs)
+export default connect(mapStateToProps, { getLogs })(Logs)
