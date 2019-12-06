abad7ea9ab2b9afd39adaa28b6dd98064c4c59b7
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-06 21:50:41 +0100

    added getLogs to proptypes

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -29,11 +29,12 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
 }
 
 Logs.propTypes = {
-  log: PropTypes.object.isRequired
+  log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
-  log: state.log
+  log: state.log,
 })
 
 export default connect(
