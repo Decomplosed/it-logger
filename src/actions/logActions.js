3757f56aeaac920e469adf4cdf68d49e007f3e65
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-06 15:50:04 +0100

    copied getLogs function so it will be a base for AddLog function

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -38,6 +38,25 @@ export const getLogs = () => async dispatch => {
   }
 }
 
+export const addLog = () => async dispatch => {
+  try {
+    setLoading()
+
+    const res = await fetch('/logs')
+    const data = await res.json()
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data
+    })
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    })
+  }
+}
+
 export const setLoading = () => {
   return {
     type: SET_LOADING
