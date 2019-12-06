a006bea657490ee9f2251f68e9b053fa040af6ee
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-06 21:49:16 +0100

    created a case of ADD_LOG

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -20,6 +20,12 @@ export default (state = initialState, action) => {
         logs: action.payload,
         loading: false
       }
+    case ADD_LOG:
+      return {
+        state,
+        log: [...state.logs, action.payload],
+        loading: false
+      }
     case SET_LOADING:
       return {
         ...state,
