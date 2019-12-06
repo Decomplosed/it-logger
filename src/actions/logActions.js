cd9a610eae1b4195fda43d284d51755c7426bbb9
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-06 23:21:45 +0100

    created a methods for setCurrent, clearCurrent and updateLog

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -3,7 +3,10 @@ import {
   SET_LOADING,
   LOGS_ERROR,
   ADD_LOG,
-  DELETE_LOG
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT
 } from './types'
 
 // export const getLogs = () => {
@@ -64,7 +67,7 @@ export const addLog = (log) => async dispatch => {
   }
 }
 
-export const deleteLog = (id) => async dispatch => {
+export const deleteLog = id => async dispatch => {
   try {
     setLoading()
 
@@ -81,6 +84,45 @@ export const deleteLog = (id) => async dispatch => {
       type: LOGS_ERROR,
       payload: err.response.data
     })
+  }
+}
+
+export const updateLog = log => async dispatch => {
+  try {
+    setLoading()
+
+    const res = await fetch(`/logs/${log.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(log),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const data = await res.json()
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data
+    })
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    })
+  }
+}
+
+export const setCurrent = log => {
+  return {
+    type: SET_CURRENT,
+    payload: log
+  }
+}
+
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT
   }
 }
 
