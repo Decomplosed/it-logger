1319ec76452db9ed60d3d2b52b6ac3578e63a986
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-08 00:20:59 +0100

    imported connect and deleteTech method

--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
+import { deleteTech } from '../../actions/techActions'
 
 const TechItem = ({ tech }) => {
   return (
