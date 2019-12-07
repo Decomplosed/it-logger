15c70791c6899f23c1ee2dbb4f32d0b37eaa72f2
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-08 00:29:43 +0100

    added onDelete function which will get deleteTech method and show a message to the user that technician was deleted

--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { deleteTech } from '../../actions/techActions'
-
 import M from 'materialize-css/dist/js/materialize.min.js'
 
 const TechItem = ({ tech: { id, firstName, lastName }, deleteTech }) => {
+  const onDelete = () => {
+    deleteTech(id)
+    M.toast({ html: 'Technician deleted' })
+  }
+
   return (
     <li className='collection-item'>
       <div>
@@ -13,7 +17,7 @@ const TechItem = ({ tech: { id, firstName, lastName }, deleteTech }) => {
         <a
           href='#!'
           className='secondary-content'
-          onClick={() => deleteTech(id)}
+          onClick={onDelete}
         >
           <i className="material-icons grey-text">delete</i>
         </a>
