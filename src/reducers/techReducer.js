0fbaf1a8d07f690608b8ffa4e7b5545250609b0c
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-07 23:07:48 +0100

    created cases for SET_LOADING and GET_TECHS

--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -14,7 +14,17 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    
+    case GET_TECHS:
+      return {
+        ...state,
+        techs: action.payload,
+        loading: false
+      }
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      }
     default:
       return state
   }
