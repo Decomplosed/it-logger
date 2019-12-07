abc966086700ef160c3083682d921ae35d993a18
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-07 23:27:25 +0100

    created a base to finish the TechSelectOptions component

---
+++ src/components/techs/TechSelectOptions.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TechSelectOptions = props => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+TechSelectOptions.propTypes = {
+
+}
+
+export default TechSelectOptions
