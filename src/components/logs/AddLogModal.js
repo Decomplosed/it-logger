b79d660474484643b9207af65af713b7f31b59c1
Bart Frackiewicz <decomplosed@gmail.com>
2019-12-07 23:38:53 +0100

    imported TechSelectOptions component to AddLogModal

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import TechSelectOptions from '../techs/TechSelectOptions'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { addLog } from '../../actions/logActions'
